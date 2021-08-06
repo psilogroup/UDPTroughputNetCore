e611a0700d195b5945153b94af2621726153e608
Gabriel <gabriel.moreira@psilogroup.com>
2021-08-06 09:44:41 -0300

    Initial commit

---
+++ UDPClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buffer = new Byte[200];
+            
+            for (byte i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = i;
+            }
+            string ip = args[0];
+            int port = int.Parse(args[1]);
+            
+            UdpClient udpClient = new UdpClient();
+            
+            while (true)
+            {
+                udpClient.Send(buffer, buffer.Length - 1,ip,port);
+            }
+        }
+    }
+}
---
+++ UDPTroughput/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPTroughput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string port = args[0];
+            
+            UdpClient receivingUdpClient = new UdpClient(int.Parse(port));
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Stopwatch watch = new Stopwatch();
+            long lastSecond = 0;
+            long currentSecond = 0;
+            long packets = 0;
+            long packets2 = 0;
+            long bytesRead = 0;
+            long bytesRead2 = 0;
+            int seconds = 0;
+            Console.WriteLine("Waiting for packets");
+            var dummy = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+            watch.Start();
+            Console.WriteLine("Packet received, starting benchmark");
+            Console.WriteLine("");
+            
+            while (seconds < 60)
+            {
+                Byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                currentSecond = watch.ElapsedMilliseconds;
+                packets++;
+                bytesRead += receiveBytes.Length;
+                
+                if (currentSecond - lastSecond > 1000)
+                {
+                    lastSecond = currentSecond;
+                    
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    var x = (packets - packets2);
+                    var y = (bytesRead - bytesRead2);
+                    Console.WriteLine("PPS: "+x+" - " + (y/(1024))+ " KB/s");
+                    packets2 = packets;
+                    bytesRead2 = bytesRead;
+                    seconds++;
+
+                }
+            }
+            Console.WriteLine($"Benchmark done: Mbs Received {bytesRead/(1024*1024)} Packets: {packets} Time: {seconds} seconds");
+        }
+    }
+}
